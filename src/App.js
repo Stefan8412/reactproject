266cdff861739f3549464fa845313f9b47e11b27
Stefan Hancar <stefanhancar@MacBook-Air-uzivatela-Stefan.local>
2020-04-21 13:48:00 +0200

    reuable update

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ class App extends Component {
        <h1>Hi, my name is Stefan Hancar</h1>
        <p>I live in Presov</p>
        <Person />
+       <Person />
+       <Person />
       </div>);
    //  return React.createElement('div',{className:'App'},React.createElement('h1',null,'Hi,I \'m Stefan'));
   }
