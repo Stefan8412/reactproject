e20c4a3a893045a3e57791e23dbba4f9dac94b43
Stefan Hancar <stefanhancar@MacBook-Air-uzivatela-Stefan.local>
2020-04-22 09:30:41 +0200

    adding css

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import "./Person.css";
 
 const person =(props)=>{
 
 return (
-    <div>
+    <div className="Person">
 <p>I am {props.name} ,reliable Web Developer and I am {props.age}years old</p>
 <p>{props.children}</p>
 </div>)
