49ba9b89904b1dd54cb39c25b740e93525c7d4ab
Stefan Hancar <stefanhancar@MacBook-Air-uzivatela-Stefan.local>
2020-04-22 07:49:55 +0200

    update

--- src/App.js
+++ src/App.js
@@ -4,14 +4,31 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state ={
+    persons:[
+      {name:"Stefan",age:25},
+      {name:"Martin",age:22},
+      {name:"Peter",age:22},
+    ]
+  }
+  switchNameHandler = () =>{
+    this.setState({persons:[
+      {name:"Steve",age:25},
+      {name:"Martin",age:22},
+      {name:"Peter",age:22}
+    ]
+
+    })
+  }
   render() {
     return (
      <div className="App">
-       <h1>Hi, my name is Stefan Hancar and I am {Math.floor(Math.random() * 30)} years old</h1>
+       <h1>Hi, my name is Stefan Hancar </h1>
        <p>I live in Presov</p>
-       <Person />
-       <Person />
-       <Person />
+       <button onClick={this.switchNameHandler}>Switch Name</button>
+       <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+       <Person name ={this.state.persons[1].name} age={this.state.persons[1].age}/>
+       <Person name ={this.state.persons[2].name} age={this.state.persons[2].age}>My hobbies are : programming</Person>
       </div>);
    //  return React.createElement('div',{className:'App'},React.createElement('h1',null,'Hi,I \'m Stefan'));
   }
